9c892bdfb43c6f945232eb51d4dbcc199adf49a9
baixu <baixu.@qq.com>
2021-09-07 19:01:24 +0800

    first commit

---
+++ src/main/java/com/xds/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xds.git;
+
+/**
+ * @Author Bai Xu
+ * @Date 2021/9/7 18:58
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
