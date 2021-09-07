c55bc76d9530c2b9438cbf212b19f5b08671061d
baixu <baixu.@qq.com>
2021-09-07 19:03:50 +0800

    second commit

--- src/main/java/com/xds/git/GitTest.java
+++ src/main/java/com/xds/git/GitTest.java
@@ -8,5 +8,6 @@ package com.xds.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
